1262ded6b683c905b5f393c56dfde7fc65766153
souzalucasj <souza.lucasj@gmail.com>
2023-04-09 14:06:17 -0300

    atividade02 add

---
+++ front-end/atividade2/index.js
@@ -0,0 +1,24 @@
+IMask(document.querySelector('#cc-cvv'),
+{
+    mask:'000'
+})
+
+IMask(document.querySelector ('#cc-number'),{
+    mask: '0000 0000 0000 0000'
+})
+
+IMask(document.querySelector('#cc-validity'),{
+    mask: 'MM{/}YY',
+    blocks:{
+        MM: {
+            mask: IMask.MaskedRange, 
+            from: 1,
+            to:12
+        },
+        YY:{
+            mask: IMask.MaskedRange,
+            from: String(new Date().getFullYear()).slice(2),
+            to: String(new Date().getFullYear()+10).slice(2)
+        }
+    }
+})
